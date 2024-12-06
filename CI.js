c36b9a5dc36c6550b603401884ad34962e24ad36
jdillon96 <143670941+jdillon96@users.noreply.github.com>
2024-12-05 16:52:45 -0800

    Initial commit

---
+++ CI.js
@@ -0,0 +1,28 @@
+const PrincipalBox = document.querySelector("#principal");
+const InterestSlider = document.querySelector("#interest");
+const FrequencyDropdown = document.querySelector("#frequency");
+const TimeBox = document.querySelector("#time");
+const profitOutput = document.querySelector("#profit");
+const totalOutput = document.querySelector("#total");
+const interestOutput = document.querySelector("#intout");
+const Button = document.querySelector("#calculate");
+
+interestOutput.textContent = InterestSlider.value; //shows the initial interest rate
+InterestSlider.addEventListener("input", (e) => {
+  interestOutput.textContent = InterestSlider.value;
+});
+Button.addEventListener("click", updateCash); //activates calculation once the button is pushed
+
+function updateCash() {
+  const principal = Number(PrincipalBox.value);
+  const interest = Number(InterestSlider.value);
+  const frequency = Number(FrequencyDropdown.value);
+  const time = Number(TimeBox.value);
+  const inside = 1 + interest / frequency / 100;
+  const exponent = frequency * time;
+  const parentheses = Math.pow(inside, exponent);
+  const total = principal * parentheses;
+  const profit = total - principal;
+  totalOutput.textContent = total.toFixed(2);
+  profitOutput.textContent = profit.toFixed(2);
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Compound-Interest-Calculator
-Created with CodeSandbox
+# Interest Computer
+
+A simple, static webapp that calculates compound interest.
+
+It accomplishes this by having the user input the principal, interest rate, how often per year interest is compounded, and how many years the money is accumulating interest.
+
+The web app returns the amount of money that was accumulated and total amount once the "Calculate!" button is clicked.
+
+Run this program here: (https://yymm9y.csb.app/)
